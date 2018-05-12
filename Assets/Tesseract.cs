cd414550f6c728d2813a10a6ec1721de22d67026
Catherine Shing <catherineshing9810@gmail.com>
2018-05-13 01:24:40 +0300

    Revert "commit"
    This reverts commit 413bd33764630ff3120955b02301de44ff860ba8.

--- Assets/Tesseract.cs
+++ Assets/Tesseract.cs
@@ -1,17 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿@ -4,32 +4,21 @@ using UnityEngine;
 
 
 public class Tesseract : MonoBehaviour {
+<<<<<<< HEAD
      public Color ObjectColor;
  
      private Color currentColor;
      private Material materialColored;
+=======
+>>>>>>> parent of 413bd33... commit
 
 	// Use this for initialization
 	void Start () {
 		Debug.Log("Start... :)");
+	
 	}
 	
 	// Update is called once per frame
@@ -31,5 +33,6 @@ public class Tesseract : MonoBehaviour {
 		// rb.AddForce(Vector3.up * 10f);
 		transform.Rotate(Vector3.right * 10f);
 		transform.Rotate(Vector3.up, Time.deltaTime, Space.World);
+		
 	}
 }
