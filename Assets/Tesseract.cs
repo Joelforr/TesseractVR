413bd33764630ff3120955b02301de44ff860ba8
Catherine Shing <catherineshing9810@gmail.com>
2018-05-12 15:13:31 -0400

    commit

--- Assets/Tesseract.cs
+++ Assets/Tesseract.cs
@@ -4,14 +4,33 @@ using UnityEngine;
 
 
 public class Tesseract : MonoBehaviour {
+     public Color ObjectColor;
+ 
+     private Color currentColor;
+     private Material materialColored;
+
 
 	// Use this for initialization
 	void Start () {
-	
+		Debug.Log("Start... :)");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+         if (ObjectColor != currentColor)
+         {
+             //helps stop memory leaks
+             if (materialColored != null)
+                 UnityEditor.AssetDatabase.DeleteAsset(UnityEditor.AssetDatabase.GetAssetPath(materialColored));
+ 
+             //create a new material
+             materialColored = new Material(Shader.Find("Diffuse"));
+             materialColored.color = currentColor = ObjectColor;
+             this.GetComponent<Renderer>().material = materialColored;
+         }
+		// Rigidbody rb = GetComponent<Rigidbody>();
+		// rb.AddForce(Vector3.up * 10f);
+		transform.Rotate(Vector3.right * 10f);
+		transform.Rotate(Vector3.up, Time.deltaTime, Space.World);
 	}
 }
