30884fda5c10bf39eb192b1560541cb248e7bab3
Catherine Shing <catherineshing9810@gmail.com>
2018-05-13 23:59:25 +0300

    Revert "Revert "commit""
    This reverts commit cd414550f6c728d2813a10a6ec1721de22d67026.

---
+++ Assets/Tersseract2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tersseract2 : MonoBehaviour {
+     public Color ObjectColor;
+ 
+     private Color currentColor;
+     private Material materialColored;
+	 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (ObjectColor != currentColor)
+         {
+             //helps stop memory leaks
+             if (materialColored != null)
+                 UnityEditor.AssetDatabase.DeleteAsset(UnityEditor.AssetDatabase.GetAssetPath(materialColored));
+ 
+             //create a new material
+             materialColored = new Material(Shader.Find("Diffuse"));
+             materialColored.color = currentColor = ObjectColor;
+             this.GetComponent<Renderer>().material = materialColored;
+         }
+
+		transform.Rotate(Vector3.left * 10f);
+		transform.Rotate(Vector3.up, Time.deltaTime, Space.World);
+	}
+}
